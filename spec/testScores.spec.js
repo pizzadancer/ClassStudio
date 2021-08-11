838f6730f64734bf6f797009c717896d263f3678
dlm <55953676+pizzadancer@users.noreply.github.com>
2021-08-11 19:39:18 +0000

    Without Testin

--- spec/testScores.spec.js
+++
@@ -1,8 +0,0 @@
-const CrewCandidate = require('../testScores.js');
-
-describe("hello" , function() {
-
-  it("should return average when stuff is done", function() {
-    expect(merryMaltese.getAverage()).toEqual(92.7);
-  })
-});
